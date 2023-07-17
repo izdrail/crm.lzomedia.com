5b38ba45265a1e23d5f71a310ce493c79426e33a
Stefan <bogdan.izdrail@gmail.com>
2023-07-17 21:03:11 +0100

    Removed secret

--- config/news.php
+++ config/news.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'news-api-key' => env('NEWS_API_KEY', 'c29a123962034057aac547e7321be062'),
+    'news-api-key' => env('NEWS_API_KEY', ''),
     'extractor' => env('NEWS_EXTRACTOR', 'http://172.16.238.20:8000/'),
 ];
